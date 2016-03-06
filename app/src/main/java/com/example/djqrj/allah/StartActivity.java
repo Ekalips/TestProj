6fcf53e0b16c24c9081aa9ecc92ded2777300641
Ekalips <djqrj96@gmail.com>
2016-03-06 21:28:39 +0200

    Image

--- app/src/main/java/com/example/djqrj/allah/StartActivity.java
+++ app/src/main/java/com/example/djqrj/allah/StartActivity.java
@@ -1,13 +1,60 @@
 package com.example.djqrj.allah;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class StartActivity extends AppCompatActivity {
+
+    private static final int SELECT_PICTURE = 1;
+
+    private String selectedImagePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Button btn = (Button) findViewById(R.id.GoBtn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*").setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Picture"), SELECT_PICTURE);
+            }
+        });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE)
+            {
+                Uri selectedImageUri = data.getData();
+                selectedImagePath = getPath(selectedImageUri);
+                ((ImageView)findViewById(R.id.imageView)).setImageURI(selectedImageUri);
+            }
+        }
+    }
+
+    private String getPath(Uri selectedImageUri) {
+        if (selectedImageUri == null) return  null;
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = managedQuery(selectedImageUri,proj,null,null,null);
+        if (cursor != null)
+        {
+            int collInd = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(collInd);
+        }
+        return selectedImageUri.getPath();
     }
 }
